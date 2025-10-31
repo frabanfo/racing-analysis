284881adbdca6d862c1aa896d4208054197807fd
frabanfo <131880693+frabanfo@users.noreply.github.com>
2025-10-31 11:28:17 +0100

    brake analysis: basic implementation

--- src/run_example.py
+++ src/run_example.py
@@ -1,18 +1,31 @@
 from src.data_load import load_fastf1_session, get_fastest_lap_telemetry
-from src.viz import plot_speed_distance
+from src.viz import plot_speed_distance, plot_throttle_distance, plot_brakes_distance
+import matplotlib
+from pathlib import Path
+matplotlib.use('tkagg')   
 
 def main():
     year = 2024
     gp = "Monaco"
     driver = "VER"
-
-    print(f"Loading session {year} {gp} ...")
     session = load_fastf1_session(year, gp, "R")
-    print("Session loaded.")
     tel = get_fastest_lap_telemetry(session, driver)
-    print("Telemetry loaded. Plotting...")
-    plt = plot_speed_distance(tel, title=f"{driver} fastest lap {gp} {year}")
-    plt.show()
+
+    fig = plot_speed_distance(tel, title=f"{driver} fastest lap {gp} {year}")
+    fig1 = plot_throttle_distance(tel, title=f"{driver} throttle in fastest lap {gp} {year}")
+    fig2 = plot_brakes_distance(tel, title=f"{driver} brakes in fastest lap {gp} {year}")
+
+    out_path = Path.cwd() / "data" / "plots"
+    out_path.mkdir(parents=True, exist_ok=True)
+    
+    file = out_path / "ver_fastest_lap.png"
+    file1 = out_path / "ver_throttle_lap.png"
+    file2 = out_path / "ver_brakes_lap.png"
+
+    fig.savefig(file, dpi=150)
+    fig1.savefig(file1, dpi=150)
+    fig2.savefig(file2, dpi=150)
+    print(f"Saved plot to {file}")
 
 if __name__ == "__main__":
     main()
--- src/viz.py
+++ src/viz.py
@@ -7,3 +7,19 @@ def plot_speed_distance(telemetry, title="Speed vs Distance"):
     ax.set_ylabel('Speed (km/h)')
     ax.set_title(title)
     return fig
+
+def plot_throttle_distance(telemetry, title="Throttle vs Distance"):
+    fig, ax = plt.subplots(figsize=(12,4))
+    ax.plot(telemetry['Distance'], telemetry['Throttle'])
+    ax.set_xlabel('Distance (m)')
+    ax.set_ylabel('Throttle (%)')
+    ax.set_title(title)
+    return fig
+
+def plot_brakes_distance(telemetry, title="Throttle vs Distance"):
+    fig, ax = plt.subplots(figsize=(12,4))
+    ax.plot(telemetry['Distance'], telemetry['Brake'])
+    ax.set_xlabel('Distance (m)')
+    ax.set_ylabel('Brakes (%)')
+    ax.set_title(title)
+    return fig
